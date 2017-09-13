c83191b8f957f338d8a89a241cbcb10ba82cc7b5
Ian Philpot <ian.philpot@microsoft.com>
2017-09-13 14:03:42 -0400

    Cleanup

---
+++ mount_azure_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo yum install samba-client samba-common cifs-utils
+mkdir mymountpoint
+sudo bash -c 'echo "//<storage-account-name>.file.core.windows.net/<share-name> /mymountpoint cifs vers=3.0,username=<storage-account-name>,password=<storage-account-key>,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
