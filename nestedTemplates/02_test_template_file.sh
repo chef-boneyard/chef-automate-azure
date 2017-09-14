3f4dc03ad5ce1b80c0c8e8a0dec90d2f0269b8cb
James Truitt <jamestruitt@mac.com>
2017-09-14 12:06:03 -0400

    sdsdf

--- nestedTemplates/02_test_template_file.sh
+++ nestedTemplates/02_test_template_file.sh
@@ -2,4 +2,5 @@
 
 rg=subway
 
-az group deployment validate --resource-group $rg --template-file scaleset.json --parameters @scaleset.params.json
+#az group deployment validate --resource-group $rg --template-file scaleset.json --parameters @scaleset.params.json
+az group deployment validate --resource-group $rg --template-file azuredeploy.json --parameters @scaleset.params.json
